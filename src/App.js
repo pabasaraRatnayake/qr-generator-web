a6e725b628cb6d0220e799ed94afbf408f0bd7ec
Pabasara <pabasara.rathnayake@goodhope.co>
2023-04-10 11:49:13 +0530

    Updated component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import './App.css';
-// import QRCode from "./Components/QR-Code";
-// import QRCodeGenerator from "./Components/QR-Code";
-import QRCodeRenderer from "./Components/QR-Code";
+//import QRCodeRenderer from "./Components/QR-Code";
 import QRCodeGenerator from "./Components/QR-Code";
+//import QRCodeComponent from "./Components/QR-Code";
+import React from "react";
 
 function App() {
   return (
       <div className="App">
+          <h1>QR Code Generator</h1>
           <QRCodeGenerator/>
       </div>
   );
 }
 
 export default App;
+
+//<QRCodeComponent text="Hello, world!" />
